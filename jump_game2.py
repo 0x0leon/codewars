119978e6e2c0cec8496d411d0b3591357a02d0ca
Leon Selz <leon.selz@sick.de>
2024-02-07 14:19:42 +0100

    test

---
+++ jump_game2.py
@@ -0,0 +1,24 @@
+
+case1 = [2,3,1,1,4]
+case2 = [2,3,0,1,4]
+
+class Solution:
+
+
+    def jump(self, nums) -> int:
+        
+        def find_way(self, index, lst):
+            if index >= len(list)-1:
+                return "geht"
+            n = list[index]
+            find_way(n, lst)
+
+
+        d = find_way(0, nums)
+        print(d)
+
+
+
+
+Solution.jump(case1)
+Solution.jump(case2)
